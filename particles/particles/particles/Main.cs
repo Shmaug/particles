384e8704ce237c10cfe3f4691d8a89d80be4b4cf
Trevor Hedstrom <trevorhedstrom@gmail.com>
2014-11-09 05:35:48 -0800

    First commit

---
+++ particles/particles/particles/Main.cs
@@ -0,0 +1,145 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace particles
+{
+    public class Main : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public static int screenWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+        public static int screenHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+
+        public const double G = 6.67e-6;
+
+        public static Texture2D circleTexture;
+
+        public static MouseState ms, lastms;
+
+        public static Particle p;
+
+        public Main()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferWidth = screenWidth;
+            graphics.PreferredBackBufferHeight = screenHeight;
+            this.IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            circleTexture = new Texture2D(GraphicsDevice, 512, 512);
+            Color[] color = new Color[512 * 512];
+            for (int x = 0; x < 512; x++)
+            {
+                for (int y = 0; y < 512; y++)
+                {
+                    float d = Vector2.Distance(new Vector2(256, 256), new Vector2(x, y));
+                    if (d < 254)
+                        color[x + y * 512] = Color.White;
+                    else if (d < 256)
+                        color[x + y * 512] = Color.Lerp(Color.White, Color.Transparent, (d-254) / 2);
+                    else
+                        color[x + y * 512] = Color.Transparent;
+                }
+            }
+            circleTexture.SetData<Color>(color);
+        }
+
+        protected override void UnloadContent()
+        {
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            ms = Mouse.GetState();
+
+            if (ms.LeftButton == ButtonState.Pressed)
+            {
+                if (p == null)
+                    p = new Particle(new Vector2(ms.X, ms.Y), Vector2.Zero, 100);
+                if (ms.ScrollWheelValue > lastms.ScrollWheelValue)
+                    if (Keyboard.GetState().IsKeyDown(Keys.LeftControl))
+                        p.mass += 1;
+                    else
+                        p.mass += 5;
+                else if (ms.ScrollWheelValue < lastms.ScrollWheelValue)
+                    if (Keyboard.GetState().IsKeyDown(Keys.LeftControl))
+                        p.mass -= 1;
+                    else
+                        p.mass -= 5;
+                if (p.mass < 1)
+                    p.mass = 1;
+            }
+            else
+            {
+                if (p != null)
+                {
+                    p.mass = p.mass * (10 ^ 24);
+                    for (int i = 0; i < Particle.particles.Length; i++)
+                    {
+                        if (Particle.particles[i] == null)
+                        {
+                            Particle.particles[i] = p;
+                            break;
+                        }
+                    }
+                    p = null;
+                }
+            }
+
+            Particle.Update(gameTime);
+
+            lastms = ms;
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            spriteBatch.Begin();
+            if (p != null)
+                spriteBatch.Draw(circleTexture,
+                    new Rectangle((int)(p.position.X - p.mass / 4),
+                                  (int)(p.position.Y - p.mass / 4),
+                                  (int)(p.mass / 2),
+                                  (int)(p.mass / 2)),
+                    null,Color.White);
+            
+            for (int i = 0; i < Particle.particles.Length; i++)
+            {
+                if (Particle.particles[i] != null)
+                {
+                    spriteBatch.Draw(circleTexture,
+                        new Rectangle((int)(Particle.particles[i].position.X - (Particle.particles[i].mass / (10 ^ 24)) / 4),
+                                      (int)(Particle.particles[i].position.Y - (Particle.particles[i].mass / (10 ^ 24)) / 4),
+                                      (int)(Particle.particles[i].mass / (10 ^ 24)) / 2,
+                                      (int)(Particle.particles[i].mass / (10 ^ 24)) / 2),
+                        null, Particle.particles[i].color);
+                }
+            }
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ particles/particles/particles/Particle.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace particles
+{
+    public class Particle
+    {
+        public static Particle[] particles = new Particle[4096];
+        
+        public Vector2 position;
+        public Vector2 velocity;
+        public Color color;
+        public double mass;
+
+        public Particle(Vector2 p, Vector2 v, double mass = 1)
+        {
+            this.position += p;
+            this.velocity += v;
+            Random r = new Random();
+            this.color = new Color(r.Next(0, 255), r.Next(0, 255), r.Next(0, 255));
+            this.mass = 1;
+        }
+
+        private void upd8(GameTime t)
+        {
+            Vector2 Fnet = Vector2.Zero;
+            for (int i = 0; i < particles.Length; i++)
+            {
+                if (particles[i] != null && particles[i] != this)
+                {
+                    double mg = Main.G * (particles[i].mass * this.mass) / Math.Pow(Vector2.Distance(particles[i].position, this.position), 2);
+                    Vector2 Fg = particles[i].position - this.position;
+                    Fg.Normalize();
+                    Fg *= (float)mg;
+                    Fnet += Fg;
+                }
+            }
+            this.velocity += Fnet / (float)this.mass;
+            this.position += this.velocity * 100 * (float)t.ElapsedGameTime.TotalMilliseconds;
+
+            for (int i = 0; i < particles.Length; i++)
+            {
+                if (particles[i] != null && particles[i] != this)
+                {
+                    double mg = Main.G * (particles[i].mass * this.mass) / Math.Pow(Vector2.Distance(particles[i].position, this.position), 2);
+                    Vector2 Fg = particles[i].position - this.position;
+                    Fg.Normalize();
+                    Fg *= (float)mg;
+                    Fnet += Fg;
+                }
+            }
+        }
+
+        public static void Update(GameTime t)
+        {
+            for (int i = 0; i < particles.Length; i++)
+            {
+                if (particles[i] != null)
+                {
+                    particles[i].upd8(t);
+                }
+            }
+        }
+    }
+}
